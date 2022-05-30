987ecadfa7de96c0070e17d81737ef5e22b4be69
Batya Tonny <batztonnie@gmail.com>
2022-05-30 11:07:12 +0300

    print hello world

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 number = 98
-print("{:d} Battery street".format(number))
+print(f"{number} Battery street")
--- 0x00-python-hello_world/README.md
+++ 0x00-python-hello_world/README.md
@@ -1,1 +1,1 @@
-# Hello world
+Starting Python
