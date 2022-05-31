aadbc790fe36b793601ba751d7901186eb5e4090
Batya Tonny <batztonnie@gmail.com>
2022-05-31 14:05:53 +0300

    hello

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for decimal in range(0, 100):
-    if decimal == 99:
-        print("{}".format(decimal), end='\n')
-    else:
-        print("{:02}".format(decimal), end=', ')
--- 0x01-python-if_else_loops_functions/README.md
+++
@@ -1,3 +0,0 @@
-# Conditional Blocks and loops.
-
-In this project, I explore the if-else conditional statements and loops.
