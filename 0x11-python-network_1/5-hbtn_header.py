65e24707a73ab3ab2d3a2bf24529bce2032b2870
devbxtzz <batztonnie@gmail.com>
2022-09-23 12:24:26 +0300

    Python Networking 1

--- 0x11-python-network_1/5-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''task 5 script'''
-
-if __name__ == '__main__':
-    import requests
-    import sys
-
-    res = requests.get(sys.argv[1])
-    print(res.headers.get('X-Request-Id'))
